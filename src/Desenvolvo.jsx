19dc7310d93b757a1395b1595f83370d72fce967
Strifexss <rocombole0007@gmail.com>
2023-01-17 23:05:58 -0300

    #

--- src/Desenvolvo.jsx
+++ src/Desenvolvo.jsx
@@ -4,7 +4,7 @@ import IconPc from "./imgs/Icons/DesignerPcIcon.png"
 import DesignerIcon from "./imgs/Icons/DesignerIcon.png"
 import IdentidadeVisualIcon from "./imgs/Icons/IdentidadeVisual.png"
 import ApresentacaoIcon from "./imgs/Icons/ApresentacaoIcon.png"
-
+import { Fade } from "react-reveal"
 const Main = styled.div`
     height: 100vh;
     width: 100%;
@@ -28,6 +28,7 @@ const Componentes = styled.div`
       display: grid;
       grid-template-columns: 50% 50%;
       grid-template-rows: 50% 50%;
+      padding: 3rem;
     
 `
 
@@ -52,13 +53,14 @@ export default function Desenvolvo() {
         <Main id="Desenvolvo">
             <section>
                 <h3>Desenvolvo</h3>
+                <Fade top>
                 <Componentes>
                     <DesenvolvoComponents nome="Design Digital" numero="1" imagem={IconPc}/>
                     <DesenvolvoComponents nome="Design Gráfico" numero="2" imagem={DesignerIcon}/>
                     <DesenvolvoComponents nome="Identidade Visuais" numero="3" imagem={IdentidadeVisualIcon}/>
                     <DesenvolvoComponents nome="Apresentações" numero="4" imagem={ApresentacaoIcon}/>
-                    
-                </Componentes>
+             </Componentes>
+             </Fade>
             </section>
             <Esquerda>
                 <h2>O que eu desenvolvo?</h2>
